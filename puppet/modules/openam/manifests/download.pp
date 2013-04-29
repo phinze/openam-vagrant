4a0d2fe7f0535d261f3d6ea982f3af229fd876ef
Jonas Fonseca <jonas.fonseca@savoirfairelinux.com>
2013-04-28 21:33:55 -0400

    cache the downloaded openam zip file
    this helps to speeds up provisioning of multiple openam instances.

--- puppet/modules/openam/manifests/download.pp
+++ puppet/modules/openam/manifests/download.pp
@@ -4,8 +4,8 @@ class openam::download {
   exec { 'download-openam':
     timeout => 0,
     command => "wget ${openam_url}",
-    cwd     => '/usr/local/src/',
-    creates => '/usr/local/src/openam_10.1.0.zip'
+    cwd     => '/vagrant',
+    creates => '/vagrant/openam_10.1.0.zip'
   }
 
   file { '/usr/local/src/openam':
@@ -16,7 +16,7 @@ class openam::download {
   }
 
   exec { 'unzip-openam':
-    command => 'unzip -d openam openam_10.1.0.zip',
+    command => 'unzip -d openam /vagrant/openam_10.1.0.zip',
     cwd     => '/usr/local/src',
     creates => '/usr/local/src/openam/openam-server-10.1.0-Xpress.war',
     require => [
